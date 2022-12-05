6f048689f3eed8d131d91a20369ef70267c5307f
nyakiri_margaret <margaret.karanja@student.moringaschool.com>
2022-12-05 17:29:52 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
   
+const inputform = document.querySelector('form');
+inputform.addEventListener('submit',(event) =>{
+    event.preventDefault();
+    console.log(event);
+    fetch ('http://localhost:3000/movies')
+    .then(res => res.json())
+    .then(data => { 
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails');
+        title.innerText = data.title;
+        summary.innerText =data.summary
+        //LOG: (3)
+    })
+});
 }
-
 document.addEventListener('DOMContentLoaded', init);
